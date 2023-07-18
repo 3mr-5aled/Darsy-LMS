b42860bd2192ef71bf263ab3e550ac109cd297cc
abdelrhman ebian <abdelrhmanebian@gmail.com>
2023-07-18 16:52:46 +0300

    asd

--- backend/models/lesson.js
+++ backend/models/lesson.js
@@ -14,6 +14,10 @@ const Lesson = new mongoose.Schema(
         type: String, 
         required:true
     },
+    description: {
+        type: String,
+        required: true,
+      },
     material:{
         name:{
             type: String,
