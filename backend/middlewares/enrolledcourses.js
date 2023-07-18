1414af6e653dbbef021c719bfe981442490545a9
abdelrhman ebian <abdelrhmanebian@gmail.com>
2023-07-19 01:02:58 +0300

    as

---
+++ backend/middlewares/enrolledcourses.js
@@ -0,0 +1,7 @@
+const User = require('../models/user')
+const ApiError = require('../utils/apierror')
+require('dotenv').config()
+const authintication =async(req,res,next)=>{
+    const {user} = req 
+}
+module.exports=authintication
