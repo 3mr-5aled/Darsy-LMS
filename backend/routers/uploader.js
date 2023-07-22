a4e099c4f9c972d8d3103ec687c254c9afe214bc
abdelrhman ebian <abdelrhmanebian@gmail.com>
2023-07-22 16:05:30 +0300

    asd

--- backend/routers/uploader.js
+++ backend/routers/uploader.js
@@ -1,10 +1,9 @@
 const express=require('express')
 
 const videouploader = require('../controllers/videouploader')
-const upload = require('../middlewares/multer')
 const authorization = require('../middlewares/authorization')
 const authintication = require('../middlewares/authintication')
-const uploadFile = require('../middlewares/multer')
+const uploadFile = require('../middlewares/uploader')
 const router = express.Router()
  
 router.post('/',authintication,authorization,uploadFile,videouploader) 
