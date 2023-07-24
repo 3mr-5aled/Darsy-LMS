d4787552b03f51fd1909da832c30b9c46445e62d
3mr-5aled <moroamr2005@gmail.com>
2023-07-24 17:21:49 +0300

    expiredDate turned off

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,41 +1,53 @@
-const asynchandler= require('express-async-handler')
-const ApiError = require('../utils/apierror')
-const Order = require('../models/order')
-const Course = require('../models/course')
-const User = require('../models/user')
-const createOrder =asynchandler(async(req,res,next)=>{
-    let {courseId} = req.body
-    const course = await Course.findById(courseId)
-    const price  = parseInt(course.price)
-    const amount = course.discount !== undefined ? price - (price * (course.discount / 100)) : price
-    const order = await Order.create({amount,userId:req.user._id,courseId})
-    req.cart_id = order._id
-    req.amount = amount
-    next()
+const asynchandler = require("express-async-handler")
+const ApiError = require("../utils/apierror")
+const Order = require("../models/order")
+const Course = require("../models/course")
+const User = require("../models/user")
+const createOrder = asynchandler(async (req, res, next) => {
+  let { courseId } = req.body
+  const course = await Course.findById(courseId)
+  const price = parseInt(course.price)
+  const amount =
+    course.discount !== undefined
+      ? price - price * (course.discount / 100)
+      : price
+  const order = await Order.create({ amount, userId: req.user._id, courseId })
+  req.cart_id = order._id
+  req.amount = amount
+  next()
 })
-const checkOrder =asynchandler(async(req,res,next)=>{
-    const order = await Order.findById(req.body.cart_id)
-    const user = await User.findById(order.userId)
-    const {response_code} = req.body.payment_result
-    if (response_code != 200) {
-       await Order.findByIdAndDelete(req.body.cart_id)
-       return next(ApiError('Payment failed',9023,400))
-    }
-    const courses = user.enrolledCourse.filter(course => course.courseId.toString() === order.courseId.toString())
-    const course = await Course.findById(order.courseId)
-    if(courses.length > 0){
-        const expiredDate = Date.now() + course.expiredTime * 24 * 60 * 60 * 1000
-        user.enrolledCourse.map(course => course.courseId.toString() === order.courseId.toString() ? course.expiredDate = expiredDate : course) 
-        await user.save()
-        order.status = 'paid'
-        await order.save()
-        return res.status(200).json({user,order})
-    }
-    const expiredDate = Date.now() + course.expiredTime * 24 * 60 * 60 * 1000
-    user.enrolledCourse.push({courseId:order.courseId,lessonsDone:[],expiredDate,name:course.name,courseImg:course.courseImg,lessonTotal:course.total})
+const checkOrder = asynchandler(async (req, res, next) => {
+  const order = await Order.findById(req.body.cart_id)
+  const user = await User.findById(order.userId)
+  const { response_code } = req.body.payment_result
+  if (response_code != 200) {
+    await Order.findByIdAndDelete(req.body.cart_id)
+    return next(ApiError("Payment failed", 9023, 400))
+  }
+  const courses = user.enrolledCourse.filter(
+    (course) => course.courseId.toString() === order.courseId.toString()
+  )
+  const course = await Course.findById(order.courseId)
+  if (courses.length > 0) {
+    // const expiredDate = Date.now() + course.expiredTime * 24 * 60 * 60 * 1000
+    // user.enrolledCourse.map(course => course.courseId.toString() === order.courseId.toString() ? course.expiredDate = expiredDate : course)
     await user.save()
-    order.status = 'paid'
+    order.status = "paid"
     await order.save()
-    res.status(200).json({user,order})
+    return res.status(200).json({ user, order })
+  }
+  const expiredDate = Date.now() + course.expiredTime * 24 * 60 * 60 * 1000
+  user.enrolledCourse.push({
+    courseId: order.courseId,
+    lessonsDone: [],
+    expiredDate,
+    name: course.name,
+    courseImg: course.courseImg,
+    lessonTotal: course.total,
+  })
+  await user.save()
+  order.status = "paid"
+  await order.save()
+  res.status(200).json({ user, order })
 })
-module.exports={createOrder,checkOrder}
+module.exports = { createOrder, checkOrder }
