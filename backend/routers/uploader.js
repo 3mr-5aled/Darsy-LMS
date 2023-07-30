53f35eab7cafefd343f88a68a6417cbfb7a34962
abdelrhman ebian <abdelrhmanebian@gmail.com>
2023-07-30 19:57:26 +0300

    upload image

--- backend/routers/uploader.js
+++ backend/routers/uploader.js
@@ -4,8 +4,10 @@ const {uploadVideo} = require('../controllers/videouploader')
 const authorization = require('../middlewares/authorization')
 const authintication = require('../middlewares/authintication')
 const uploadFile = require('../middlewares/uploader')
+const uploadImage = require('../middlewares/imageupload')
 const router = express.Router()
  
 router.post('/',authintication,authorization,uploadFile,uploadVideo) 
+router.post('/image',authintication,authorization,uploadImage) 
 
 module.exports=router
