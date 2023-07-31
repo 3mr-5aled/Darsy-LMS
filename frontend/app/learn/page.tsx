0e92f1cbf11fe205d40c939f9148e6b2d3d3edd3
3mr-5aled <moroamr2005@gmail.com>
2023-07-31 23:30:23 +0300

    fix learn page layout

--- frontend/app/learn/page.tsx
+++ frontend/app/learn/page.tsx
@@ -67,28 +67,30 @@ const StudentMainPage = () => {
     }
   }
   const renderMembershipIcon = () => {
-    if (user?.isMemberShip.name === "gold") {
+    const membershipName = user?.membership?.name?.toLowerCase()
+    if (membershipName && membershipName.includes("gold")) {
       return (
-        <div>
+        <div className="flex flex-row">
           <p className="font-bold text-amber-400">Gold</p>
           <BsCircleFill className="text-amber-400" />
         </div>
       )
-    } else if (user?.isMemberShip.name === "platinum") {
+    } else if (membershipName && membershipName.includes("platinum")) {
       return (
-        <div>
+        <div className="flex flex-row">
           <p className="font-bold text-cyan-500">Platinum</p>
           <BsStars className="text-cyan-500" />
         </div>
       )
-    } else if (user?.isMemberShip.name === "diamond") {
+    } else if (membershipName && membershipName.includes("diamond")) {
       return (
-        <div>
+        <div className="flex flex-row">
           <p className="font-bold text-blue-500">Diamond</p>
           <BsDiamondFill className="text-blue-500" />
         </div>
       )
     }
+
     return <p className="font-bold text-gray-500">No current subscription</p> // Render nothing if the membership is not gold, platinum, or diamond
   }
 
