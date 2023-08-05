977dbdafa4e7fc143ba93de21fba8dabed97084f
abdelrhman ebian <abdelrhmanebian@gmail.com>
2023-08-06 00:09:07 +0300

    quiz

--- frontend/components/QuizForm.tsx
+++ frontend/components/QuizForm.tsx
@@ -1,16 +1,16 @@
-import { Question } from "@/common.types"
-import { useEffect, useState } from "react"
-import axiosInstance from "@/axios.config"
+import { Question } from "@/common.types";
+import { useEffect, useState } from "react";
+import axiosInstance from "@/axios.config";
 import {
   BsArrowLeftCircleFill,
   BsCheckSquareFill,
   BsCircleFill,
   BsImage,
   BsXCircleFill,
-} from "react-icons/bs"
-import { toast } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import { useRouter } from "next/navigation"
+} from "react-icons/bs";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/navigation";
 
 const CreateQuizPage = ({
   courseId,
@@ -19,14 +19,14 @@ const CreateQuizPage = ({
   exam,
   type,
 }: {
-  courseId: string
-  sectionId: string
-  lessonId: string
-  exam?: Question[]
-  type: "create" | "edit"
+  courseId: string;
+  sectionId: string;
+  lessonId: string;
+  exam?: Question[];
+  type: "create" | "edit";
 }) => {
-  const router = useRouter()
-  const [imageData, setImageData] = useState<string | null>(null)
+  const router = useRouter();
+  const [imageData, setImageData] = useState<string | null>(null);
 
   const [questions, setQuestions] = useState<Question[]>([
     {
@@ -39,17 +39,17 @@ const CreateQuizPage = ({
       correctAnswer: [],
       isCheckBoxQuiz: false,
     },
-  ])
+  ]);
 
   const getImageURL = (image: File | string | null) => {
-    if (!image) return 'no image'
-    return typeof image === "string" ? image : URL.createObjectURL(image)
-  }
+    if (!image) return "no image";
+    return typeof image === "string" ? image : URL.createObjectURL(image);
+  };
 
   useEffect(() => {
     if (exam) {
       // Deep clone the exam data to avoid modifying the original data
-      setQuestions(exam)
+      setQuestions(exam);
     } else {
       // If no exam data is provided, initialize the state with a default question
       setQuestions([
@@ -63,9 +63,9 @@ const CreateQuizPage = ({
           correctAnswer: [],
           isCheckBoxQuiz: false,
         },
-      ])
+      ]);
     }
-  }, [exam])
+  }, [exam]);
 
   const addQuestion = () => {
     if (questions.length < 30) {
@@ -81,144 +81,152 @@ const CreateQuizPage = ({
           correctAnswer: [],
           isCheckBoxQuiz: false,
         },
-      ])
+      ]);
     } else {
-      toast.error("You have reached the maximum limit of 30 questions.")
+      toast.error("You have reached the maximum limit of 30 questions.");
     }
-  }
+  };
 
   const removeQuestion = (questionIndex: number) => {
     if (questions.length > 1) {
-      const exams = [...questions]
-      exams.splice(questionIndex, 1)
-      setQuestions(exams)
+      const exams = [...questions];
+      exams.splice(questionIndex, 1);
+      setQuestions(exams);
     } else {
-      toast.error("You must have at least one question in the quiz.")
+      toast.error("You must have at least one question in the quiz.");
     }
-  }
+  };
 
   const addAnswer = (questionIndex: number) => {
     if (questions[questionIndex].answers.length < 4) {
-      const exams = [...questions]
-      exams[questionIndex].answers.push({ text: "", image: null })
-      setQuestions(exams)
+      const exams = [...questions];
+      exams[questionIndex].answers.push({ text: "", image: null });
+      setQuestions(exams);
     }
-  }
+  };
 
   const removeAnswer = (questionIndex: number, answerIndex: number) => {
     if (questions[questionIndex].answers.length > 2) {
-      const exams = [...questions]
-      exams[questionIndex].answers.splice(answerIndex, 1)
-      setQuestions(exams)
+      const exams = [...questions];
+      exams[questionIndex].answers.splice(answerIndex, 1);
+      setQuestions(exams);
     }
-  }
+  };
 
   const handleQuestionChange = (index: number, value: string) => {
-    const exams = [...questions]
-    exams[index].question = value
-    setQuestions(exams)
-  }
+    const exams = [...questions];
+    exams[index].question = value;
+    setQuestions(exams);
+  };
 
-  const handleQuestionImageChange = (questionIndex: number, file: null | File ) => {
-    const exams = [...questions]
+  const handleQuestionImageChange = (
+    questionIndex: number,
+    file: null | File
+  ) => {
+    const exams = [...questions];
     if (file !== null) {
-      exams[questionIndex].questionImage = file
-      setQuestions(exams)
+      exams[questionIndex].questionImage = file;
+      setQuestions(exams);
     }
-  }
+  };
 
   const handleAnswerChange = (
     questionIndex: number,
     answerIndex: number,
     value: string
   ) => {
-    const exams = [...questions]
-    exams[questionIndex].answers[answerIndex].text = value
-    setQuestions(exams)
-  }
+    const exams = [...questions];
+    exams[questionIndex].answers[answerIndex].text = value;
+    setQuestions(exams);
+  };
 
   const handleAnswerImageChange = (
     questionIndex: number,
     answerIndex: number,
     file: File | null
   ) => {
-    const exams = [...questions]
+    const exams = [...questions];
     if (file) {
-      exams[questionIndex].answers[answerIndex].image = file
-      setQuestions(exams)
+      exams[questionIndex].answers[answerIndex].image = file;
+      setQuestions(exams);
     }
-  }
+  };
   const handleQuestionTypeChange = (
     questionIndex: number,
     isCheckbox: boolean
   ) => {
-    const exams = [...questions]
-    exams[questionIndex].isCheckBoxQuiz = isCheckbox
+    const exams = [...questions];
+    exams[questionIndex].isCheckBoxQuiz = isCheckbox;
     if (!isCheckbox) {
-      exams[questionIndex].correctAnswer = [] // Reset the correct answer when switching to radio button
+      exams[questionIndex].correctAnswer = []; // Reset the correct answer when switching to radio button
     }
-    setQuestions(exams)
-  }
+    setQuestions(exams);
+  };
 
   const handleCorrectAnswerChange = (
     questionIndex: number,
     answerIndex: number
   ) => {
-    const exams = [...questions]
+    const exams = [...questions];
     if (questions[questionIndex].isCheckBoxQuiz) {
-      const selectedAnswers = exams[questionIndex].correctAnswer
+      const selectedAnswers = exams[questionIndex].correctAnswer;
       const selectedAnswerText =
-        questions[questionIndex].answers[answerIndex].text
+        questions[questionIndex].answers[answerIndex].text;
       if (!selectedAnswers.includes(selectedAnswerText)) {
-        selectedAnswers.push(selectedAnswerText)
+        selectedAnswers.push(selectedAnswerText);
       } else {
-        const index = selectedAnswers.indexOf(selectedAnswerText)
-        selectedAnswers.splice(index, 1)
+        const index = selectedAnswers.indexOf(selectedAnswerText);
+        selectedAnswers.splice(index, 1);
       }
     } else {
       exams[questionIndex].correctAnswer = [
         questions[questionIndex].answers[answerIndex].text,
-      ]
+      ];
     }
-    setQuestions(exams)
-  }
+    setQuestions(exams);
+  };
 
   const handleSubmit = async () => {
-    const isEmptyQuestion = questions.some((q) => q.question.trim() === "")
+    const isEmptyQuestion = questions.some((q) => q.question.trim() === "");
     if (isEmptyQuestion) {
-      toast.error("Please fill in all questions before submitting.")
-      return
+      toast.error("Please fill in all questions before submitting.");
+      return;
     }
 
     const hasEmptyAnswer = questions.some((q) =>
       q.answers.some((answer) => answer.text.trim() === "")
-    )
+    );
     if (hasEmptyAnswer) {
-      toast.error("Please fill in all answers before submitting.")
-      return
+      toast.error("Please fill in all answers before submitting.");
+      return;
     }
 
-    const hasLessThanTwoAnswers = questions.some((q) => q.answers.length < 2)
+    const hasLessThanTwoAnswers = questions.some((q) => q.answers.length < 2);
     if (hasLessThanTwoAnswers) {
-      toast.error("Each question must have at least two answers.")
-      return
+      toast.error("Each question must have at least two answers.");
+      return;
     }
 
-    const hasNoCorrectAnswer = questions.some((q) => q.correctAnswer.length === 0)
+    const hasNoCorrectAnswer = questions.some(
+      (q) => q.correctAnswer.length === 0
+    );
     if (hasNoCorrectAnswer) {
       toast.error(
         "Please select at least one correct answer for each question."
-        )
-        return
+      );
+      return;
+    }
+    questions.forEach((q) => {
+      const answerTexts = q.answers.map((answer) => answer.text);
+      const duplicates = answerTexts.some(
+        (text, index) => answerTexts.indexOf(text) !== index
+      );
+      if (duplicates) {
+        toast.error("the answers in each question must be different");
+        return;
       }
-      questions.forEach(q =>{ 
-        const answerTexts = q.answers.map((answer) => answer.text);
-        const duplicates = answerTexts.some((text, index) => answerTexts.indexOf(text) !== index);
-        if (duplicates) {
-          return toast.error('the answers in each question must be different')
-        }
-      })
-          try {
+    });
+    try {
       // Convert images to base64 and upload
       const exams = await Promise.all(
         questions.map(async (q) => ({
@@ -235,7 +243,7 @@ const CreateQuizPage = ({
             }))
           ),
         }))
-      )
+      );
 
       // Validate that all questions have at least one correct answer
 
@@ -243,58 +251,58 @@ const CreateQuizPage = ({
       const response = await axiosInstance.put(
         `/exam/${lessonId}/add-exam`,
         exams
-      )
+      );
 
       // Handle the response from the API
       if (response.status === 200) {
-        console.log(response.data)
+        console.log(response.data);
         // Exam successfully created
-        toast.success("Quiz successfully created!")
-        router.push(`/admin/courses/manage-course/${courseId}`)
+        toast.success("Quiz successfully created!");
+        router.push(`/admin/courses/manage-course/${courseId}`);
 
         // You can also redirect the user to a different page after successful submission if needed.
       } else {
-        toast.error("Error submitting the quiz. Please try again.")
+        toast.error("Error submitting the quiz. Please try again.");
       }
     } catch (error: any) {
-      toast.error("Error submitting the quiz. Please try again.")
-      console.error(error)
+      toast.error("Error submitting the quiz. Please try again.");
+      console.error(error);
     }
-  }
+  };
 
   const uploadImageAndGetURL = async (
     image: File
   ): Promise<string | null | undefined> => {
     try {
-      const base64Image = await getBase64FromImageFile(image)
+      const base64Image = await getBase64FromImageFile(image);
       const response = await axiosInstance.post("/upload/image", {
         image: base64Image,
-      })
+      });
 
       if (response.status === 200 && response.data.url) {
-        return response.data.url
+        return response.data.url;
       } else {
-        toast.error("Error uploading image. Please try again.")
+        toast.error("Error uploading image. Please try again.");
       }
     } catch (error) {
-      toast.error("Error uploading image. Please try again.")
-      console.error(error)
+      toast.error("Error uploading image. Please try again.");
+      console.error(error);
     }
-  }
+  };
 
   const getBase64FromImageFile = (file: File) => {
     return new Promise<string | null>((resolve) => {
-      const reader = new FileReader()
-      reader.readAsDataURL(file)
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
       reader.onloadend = () => {
-        const base64String = reader.result as string
-        resolve(base64String) // Extract base64 data from the string
-      }
+        const base64String = reader.result as string;
+        resolve(base64String); // Extract base64 data from the string
+      };
       reader.onerror = () => {
-        resolve(null)
-      }
-    })
-  }
+        resolve(null);
+      };
+    });
+  };
 
   return (
     <div className="container p-4 mx-auto">
@@ -331,7 +339,10 @@ const CreateQuizPage = ({
                 accept="image/*"
                 className="z-30 w-full h-full cursor-pointer absolute opacity-0"
                 onChange={(e) =>
-                  handleQuestionImageChange(questionIndex,e.target.files ? e.target.files[0] : null)
+                  handleQuestionImageChange(
+                    questionIndex,
+                    e.target.files ? e.target.files[0] : null
+                  )
                 }
               />
               <div className="w-fit h-fit z-10 rounded-full cursor-pointer p-3 ">
@@ -481,7 +492,7 @@ const CreateQuizPage = ({
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateQuizPage
+export default CreateQuizPage;
