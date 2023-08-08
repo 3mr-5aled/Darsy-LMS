59ebb9de522298ed96564e0647683329fa0ba1a5
3mr-5aled <moroamr2005@gmail.com>
2023-08-08 12:10:33 +0300

    132

--- frontend/axios.config.ts
+++ frontend/axios.config.ts
@@ -1,11 +1,14 @@
 import axios from "axios"
 
 const isProduction = process.env.NODE_ENV === "production"
+// const baseUrl = isProduction
+//   ? `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}` // Set your production base URL
+//   : `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}` // Set your development base URL
 const baseUrl = isProduction
-  ? process.env.NEXT_PUBLIC_BACKEND_BASE_URL // Set your production base URL
+  ? `https://darsy-lms-7ni9.onrender.com` // Set your production base URL
   : `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}` // Set your development base URL
 
-console.log("baseUrl:", baseUrl) // Add this line to log the baseUrl
+console.log("baseUrl:", process.env.NEXT_PUBLIC_BACKEND_BASE_URL) // Add this line to log the baseUrl
 
 const axiosInstance = axios.create({
   baseURL: `${baseUrl}/api/v1/`, // Set your base URL
