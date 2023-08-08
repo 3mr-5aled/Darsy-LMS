550a411a6161eb43332333945385857b72eb0072
3mr-5aled <moroamr2005@gmail.com>
2023-08-08 11:42:29 +0300

    1123

--- frontend/axios.config.ts
+++ frontend/axios.config.ts
@@ -5,6 +5,8 @@ const baseUrl = isProduction
   ? process.env.NEXT_PUBLIC_BACKEND_BASE_URL || ""
   : `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}`
 
+console.log(baseUrl)
+
 const axiosInstance = axios.create({
   baseURL: `${baseUrl}/api/v1/`, // Set your base URL
   withCredentials: true,
