e2a7169d86368c4599c71a26165648c0cd9ffc0d
AbdelrhmanEbian <116501838+AbdelrhmanEbian@users.noreply.github.com>
2023-08-09 00:24:20 +0300

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -32,7 +32,7 @@ const login = asynchandler(async (req, res, next) => {
   const token = jwt.sign({..._doc},process.env.JWT );
   // send response with all user details and token as cookie
   const {name,email,phone,parentsPhone,grade,city,gender,role,enrolledCourse} = user
-  res.status(201).cookie("token", token,{ secure: true, sameSite: 'none'  }).json({email,name,phone,parentsPhone,grade,city,gender,role,enrolledCourse})
+  res.status(201).cookie("token", token).json({email,name,phone,parentsPhone,grade,city,gender,role,enrolledCourse})
 });
 
 const register = asynchandler(async (req, res, next) => {
