626af1d790237bfef59ed15522e573846a3d4f5a
3mr-5aled <moroamr2005@gmail.com>
2023-08-16 14:12:57 +0300

    add tawk chatbot

---
+++ frontend/components/Features/TawkToWidget.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { useEffect } from "react"
+
+const TawkToWidget: React.FC = () => {
+  useEffect(() => {
+    const script = document.createElement("script")
+    script.async = true
+    script.src = "https://embed.tawk.to/64dca6e894cf5d49dc6ab457/1h7uunv78"
+    script.charset = "UTF-8"
+    script.setAttribute("crossorigin", "*")
+    document.getElementsByTagName("head")[0].appendChild(script)
+
+    return () => {
+      document.getElementsByTagName("head")[0].removeChild(script)
+    }
+  }, [])
+
+  return null
+}
+
+export default TawkToWidget
