c1add4ac555039e101a4d15d5ec3190cc6094d65
Amr Khaled Hassan <3mr5aled.dev@gmail.com>
2023-08-21 18:20:15 +0300

    Revert "Merge branch 'main' into Beta"
    This reverts commit 9ec04b930c39ed397a40c7d631a2bfcc17757086.

--- frontend/components/NotFoundComponent.tsx
+++
@@ -1,9 +0,0 @@
-const NotFoundComponent = ({ message }: { message: string }) => {
-  return (
-    <div className="flexCenter h-[calc(100vh-50px)] w-full text-2xl">
-      {message}
-    </div>
-  )
-}
-
-export default NotFoundComponent
