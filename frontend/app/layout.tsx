744bf2713b1c5619611b463a040d57b77ba98a22
Amr Khaled Hassan <3mr5aled.dev@gmail.com>
2023-08-25 23:58:47 +0300

    update packages and screen shots

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css"
 import Navbar from "@/components/Nav/Navbar"
 import Footer from "@/components/Nav/Footer"
 import { Owner } from "@/constant"
-import { ToastContainer } from "react-toastify"
+import { ToastContainer } from "react-toastify/dist"
 import "react-toastify/dist/ReactToastify.css"
 import { UserProvider } from "@/contexts/userContext"
 import ClientOnlyRoute from "@/components/Routes/ClientOnlyRoute"
@@ -12,6 +12,7 @@ import FetchUserOnLoad from "@/components/Features/FetchUserOnLoad"
 import TawkToWidget from "@/components/Features/TawkToWidget"
 import TawkOnlyRoute from "@/components/Routes/TawkOnlyRoute"
 import { Metadata } from "next"
+import Head from "next/head"
 
 export const metadata: Metadata = {
   title: Owner.WebsiteDetails.name,
@@ -62,6 +63,59 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="ar">
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <link
+          href="splashscreens/iphone5_splash.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/iphone6_splash.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/iphoneplus_splash.png"
+          media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/iphonex_splash.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/iphonexr_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/iphonexsmax_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/ipad_splash.png"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/ipadpro1_splash.png"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/ipadpro3_splash.png"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/ipadpro2_splash.png"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+      </Head>
       <body>
         <TawkOnlyRoute>
           <TawkToWidget />
