0c2efe131372d7b8eda04b01d7bb6cae0f0edc9c
Amr Khaled Hassan <3mr5aled.dev@gmail.com>
2023-08-28 00:02:15 +0300

    12345

--- frontend/axios.config.ts
+++ frontend/axios.config.ts
@@ -2,8 +2,8 @@ import axios, { AxiosInstance } from "axios"
 
 const isProduction = process.env.NODE_ENV === "production"
 const baseUrl = isProduction
-  ? `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}` // Set your production base URL
-  : `${process.env.NEXT_PUBLIC_BACKEND_LOCAL_BASE_URL}` // Set your development base URL
+  ? process.env.NEXT_PUBLIC_BACKEND_BASE_URL // Set your production base URL
+  : process.env.NEXT_PUBLIC_BACKEND_LOCAL_BASE_URL // Set your development base URL
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: baseUrl, // Set your base URL
