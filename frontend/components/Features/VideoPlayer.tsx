15ac8142fb36c82421a8fd552a870fae7a62a84c
abdelrhman ebian <abdelrhmanebian@gmail.com>
2023-08-28 02:37:37 +0300

    blob

--- frontend/components/Features/VideoPlayer.tsx
+++ frontend/components/Features/VideoPlayer.tsx
@@ -157,7 +157,7 @@ export default function VideoPlayer({ video }: Props) {
                 type: "video",
                 sources: [
                   {
-                    src: URL.createObjectURL(videoBlob),
+                    src: "https://darsy-lms-beta.vercel.app/app/lesson/64ebcfbd69c8d50e4e0539a1",
                     type: "video/mp4",
                   },
                 ],
