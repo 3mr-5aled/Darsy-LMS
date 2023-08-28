2def0da33ac541cdb04298a067d9db542392743e
Amr Khaled Hassan <3mr5aled.dev@gmail.com>
2023-08-28 22:51:32 +0300

    minor changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,32 +1,32 @@
-"use client"
-import { Owner } from "@/constant"
-import Hero from "@/components/static/Hero"
-import { useUserContext } from "@/contexts/userContext"
-import { useRouter } from "next/navigation"
-import Testimonials from "@/components/static/Testimonials"
-import Features from "@/components/static/Features"
+"use client";
+import { Owner } from "@/constant";
+import Hero from "@/components/static/Hero";
+import { useUserContext } from "@/contexts/userContext";
+import { useRouter } from "next/navigation";
+import Testimonials from "@/components/static/Testimonials";
+import Features from "@/components/static/Features";
 
 const Home = () => {
-  const { state } = useUserContext()
-  const { user } = state
-  const router = useRouter()
+  const { state } = useUserContext();
+  const { user } = state;
+  const router = useRouter();
   if (user?.role === "student") {
-    router.push("/app")
+    router.push("/app");
   }
   return (
-    <div className="flexCenter ">
+    <div className="flexCenter">
       <div className="container flex flex-col">
-        <main className="flex flex-col justify-center overflow-hidden gap-y-5 md:gap-y-8">
+        <main className="flex flex-col justify-center gap-y-5 overflow-hidden md:gap-y-8">
           <Hero />
         </main>
-        <div className="py-24 bg-base-100 sm:py-32">
-         <Features />
+        <div className="bg-base-100 py-24 sm:py-32">
+          <Features />
         </div>
         {/* testimonials */}
         <Testimonials />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
