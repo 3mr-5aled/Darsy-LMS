8c8ccf7dc9a71ebcb0847f8e1724caf28fa1c0e5
AbdelrhmanEbian <116501838+AbdelrhmanEbian@users.noreply.github.com>
2024-08-31 13:23:14 +0300

    Update AwsUploader.tsx

--- frontend/components/Forms/AwsUploader.tsx
+++ frontend/components/Forms/AwsUploader.tsx
@@ -1,48 +1,76 @@
-import { useState } from "react";
-import { Upload } from "@aws-sdk/lib-storage";
-import { getSignedUrl } from"@aws-sdk/s3-request-presigner"
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import React, { useState } from "react";
+import aws from "aws-sdk";
+const s3 = new aws.S3({
+  region: process.env.NEXT_PUBLIC_REGION,
+  accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
+  secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY,
+});
+type UploadButtonProps = {
+  onVideoUpload: (url: string, publicId: string, fileName: string) => void;
+};
+const AwsUploader = ({ onVideoUpload }: UploadButtonProps) => {
+  const [uploadedObject, setUploadedObject] = useState<any>(null);
+  const [progress, setProgress] = useState<string | null>(null);
 
-function VideoUpload() {
-  const [progress, setProgress] = useState<null | string>(null);
-  const client = new S3Client({
-    region: "region",
-    credentials: {
-      accessKeyId: "",
-      secretAccessKey: "",
-    },
-  });
-  const handleFileChange = async (e: any) => {
-    const file = e.target.files[0];
-    try {
-      const parallelUploads3 = new Upload({
-        client,
-        params: { Bucket: "bucket name", Key: "file name", Body: file },
-        leavePartsOnError: false, // optional manually handle dropped parts
-      });
-
-      parallelUploads3.on("httpUploadProgress", (progress: any) => {
-        setProgress(progress.loaded / progress.total + "%");
-      });
-
-      await parallelUploads3.done();
-      const command = new GetObjectCommand({
-        Bucket: "bucket name",
-        Key: "file name",
-      });
-      const url = await getSignedUrl(client, command);
-      console.log("success");
-      return url
-    } catch (e) {
-      console.log(e);
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    console.log(file?.type);
+    if (!file?.name || file?.type !== "video/mp4") {
+      return;
+    }
+    const Key = Date.now() + file?.name;
+    if (file) {
+      try {
+        const uploadParams = {
+          Bucket: process.env.NEXT_PUBLIC_BUCKET_NAME,
+          Key,
+          Body: file,
+        };
+        const uploadedVideo = await s3
+          .upload(uploadParams as any)
+          .on("httpUploadProgress", (progress : any) => {
+            setProgress((progress.loaded / progress.total).toFixed(0));
+          })
+          .promise();
+        console.log(uploadedVideo);
+        onVideoUpload(
+          uploadedVideo.Location,
+          uploadedVideo.Key,
+          uploadedVideo.Key,
+        );
+        setProgress("100");
+      } catch (error) {
+        console.error("Error uploading file:", error);
+      }
     }
   };
 
   return (
-    <div>
-      <input type="file" hidden onChange={(e) => handleFileChange(e as any)} />
-    </div>
+    <>
+      {progress === null ? (
+        <label className="btn btn-primary">
+          Upload a Video
+          <input
+            title="video-upload"
+            className="input input-bordered"
+            type="file"
+            hidden
+            onChange={handleFileChange}
+          />
+        </label>
+      ) : (
+        <div className=" flex items-center gap-2">
+          <progress
+            className="progress progress-primary h-10 w-full bg-transparent"
+            value={progress}
+            max="100"
+          ></progress>
+          <span>{progress}%</span>
+        </div>
+      )}
+    </>
   );
-}
+};
 
-export default VideoUpload;
+export default AwsUploader;
+
