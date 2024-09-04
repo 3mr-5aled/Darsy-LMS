1acc9e160baf45ea3ff32c507548d96c10ea31df
abdelrhman ebian <abdelrhmanebian@gmail.com>
2024-09-04 17:35:50 +0300

    token

--- backend/middlewares/authintication.js
+++ backend/middlewares/authintication.js
@@ -4,6 +4,7 @@ const ApiError = require('../utils/apierror')
 require('dotenv').config()
 const authintication =async(req,res,next)=>{
     const {token}=req.cookies
+    console.log(token);
     if (!token) {
         return next(new ApiError("please login or register",3824,401)) 
     }
